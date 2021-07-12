b77b980e7e92d2376cf4f8cc3a489cfe4211b82c
Avratz <signos97@gmail.com>
2021-07-12 00:51:16 -0300

    https

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import Axios from 'axios'
 
 const api = Axios.create({
-	baseURL: 'http://private-8e8921-woloxfrontendinverview.apiary-mock.com',
+	baseURL: 'https://private-8e8921-woloxfrontendinverview.apiary-mock.com',
 	responseType: 'json',
 	method: 'get',
 	timeout: 10000,
